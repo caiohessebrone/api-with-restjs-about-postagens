c4459d7586c8321f8558fceea788407580c0ab90
caiohessebrone <caiobrone77@gmail.com>
2021-09-15 13:49:41 -0300

    Autenticacao jwt em todas as rotas de postagens

--- src/postagens/postagens.controller.ts
+++ src/postagens/postagens.controller.ts
@@ -1,5 +1,5 @@
 import { JwtAuthGuard } from './../auth/shared/jwt-auth.guard';
-import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Post, Put, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { Postagem } from './shared/postagem';
 import { PostagemService } from './shared/postagem.service';
 
@@ -14,21 +14,25 @@ export class PostagensController {
         return this.postagemService.getAll();
     }
 
+    @UseGuards(JwtAuthGuard)
     @Get('/autor/:name')
     async getByAutor(@Param('name') name: string): Promise<Postagem[]> {
         return this.postagemService.getByAutor(name);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Get(':id')
     async getById(@Param('id') id: string): Promise<Postagem> {
         return this.postagemService.getById(id);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Post()
     async create(@Body() postagem: Postagem): Promise<Postagem> {
         return this.postagemService.create(postagem);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Put(':id')
     async atualizar(
             @Param('id') id: string, 
@@ -37,6 +41,7 @@ export class PostagensController {
         return this.postagemService.update(id, postagem);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Delete(':id')
     deletar(@Param('id') id: string ) {
         this.postagemService.deletar(id);
