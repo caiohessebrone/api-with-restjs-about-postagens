c6c362a8c4ecabca746cb866a320db98c3ce50e9
Douglas <douglas_n11@hotmail.com>
2021-09-15 00:02:38 -0300

    Foi configurado uma rota de usuarios e criado um schema pro BD e adicionado as funcionalidades básicas de um CRUD(POST,DELETE,UPDATE,GET)

--- src/users/user.module.ts
+++ src/users/user.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
 import { UserController } from "./users.controller";
 import { UserService } from "./shared/user.service";
+import { MongooseModule } from "@nestjs/mongoose";
+import { UserSchema } from "./schemas/users.schema";
 
 
 @Module({
-    imports: [],
+    imports: [MongooseModule.forFeature([{ name: "Users", schema: UserSchema}])],
     controllers: [UserController],
     providers: [UserService]
 })
