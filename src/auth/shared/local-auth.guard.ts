e1e8370692891de0beb396594dc02db5e5034e0b
Douglas <douglas_n11@hotmail.com>
2021-09-17 20:11:04 -0300

    commitando para sincronizar

--- src/auth/shared/local-auth.guard.ts
+++ src/auth/shared/local-auth.guard.ts
@@ -4,7 +4,6 @@ https://docs.nestjs.com/guards#guards
 
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { Observable } from 'rxjs';
 
 @Injectable()
 export class LocalAuthGuard extends AuthGuard('local'){}
